50e46877ba12a2ba8d1095f6d159518531bcebc5
Tseng, Yun Yeh <e2483206@cmaisonneuve.qc.ca>
2026-04-07 20:22:48 -0400

    change config base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/h26-tp1-yunyeh/',
+  base: '/Le-petit-messager/',
   server: {
     open: true
   }
